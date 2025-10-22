7af56361f4d9cac1df8ae69fcbb16a96acc566fd
Yash <saiyaswanth400@gmail.com>
2025-10-22 12:06:55 +0530

    created aws resources using terraform

---
+++ backend/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "other-bucket" {
+    bucket = "myterraform-myeks-mystate-mybucket"
+    
+    lifecycle {
+      prevent_destroy = false
+    }
+
+}
+
+resource "aws_dynamodb_table" "my-table" {
+  name         = "terraform-eks-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+    
+  
+}
